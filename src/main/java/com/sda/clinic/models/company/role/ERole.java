79fc2be7eba81d64a6c1bd3727c5865d4428baa0
Łukasz Łaszek <laszek.lukasz@gmail.com>
2023-02-15 14:56:56 +0100

    Add lombok instead of boilerplate code and modify for the rest of models standard

--- src/main/java/com/sda/clinic/models/company/role/ERole.java
+++ src/main/java/com/sda/clinic/models/company/role/ERole.java
@@ -1,9 +1,9 @@
 package com.sda.clinic.models.company.role;
 
 public enum ERole {
-    ROLE_ADMIN,
+    ROLE_PUBLIC,
+    ROLE_PATIENT,
     ROLE_DOCTOR,
     ROLE_SECRETARY,
-    ROLE_PATIENT,
-    ROLE_PUBLIC
+    ROLE_ADMIN
 }
--- src/main/java/com/sda/clinic/models/company/role/Role.java
+++ src/main/java/com/sda/clinic/models/company/role/Role.java
@@ -1,6 +1,12 @@
 package com.sda.clinic.models.company.role;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "roles")
@@ -13,29 +19,5 @@ public class Role {
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
     private ERole name;
-
-    public Role() {
-
-    }
-
-    public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
-        this.name = name;
-    }
 
 }
